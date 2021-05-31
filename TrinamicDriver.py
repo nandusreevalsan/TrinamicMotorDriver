9884c37f27fe308e0e416a5e2478279410d6570f
Nandu Sreevalsan <nandu.sreevalsan@gmail.com>
2021-05-31 08:34:11 +0400

    test

--- TrinamicDriver.py
+++ TrinamicDriver.py
@@ -1,23 +1,25 @@
 #This is a script for controlling a Trinamic stepper motor using CANOpen protocol
 #Using Kvaser canlib library
-
+print("Yeah it is comimg here")
 from canlib import canlib, Frame
 
-channel = 2
-channel1 = 3
+channel = 1
+channel1 = 2
 
 chd = canlib.ChannelData(channel)
+num_channels = canlib.getNumberOfChannels()
 
+print("No of channels: ",num_channels)
 ch0 = canlib.openChannel(channel, canlib.canOPEN_ACCEPT_VIRTUAL)
 ch0.setBusOutputControl(canlib.canDRIVER_NORMAL)
-ch0.setBusParams(canlib.canBITRATE_1M)
+ch0.setBusParams(canlib.canBITRATE_100K)
 ch0.busOn()
 #ch1.busOff()
 
 
 ch1 = canlib.openChannel(channel1, canlib.canOPEN_ACCEPT_VIRTUAL)
 ch1.setBusOutputControl(canlib.canDRIVER_NORMAL)
-ch1.setBusParams(canlib.canBITRATE_1M)
+ch1.setBusParams(canlib.canBITRATE_100K)
 ch1.busOn()
 
 while True:
@@ -26,6 +28,7 @@ while True:
         #frame = Frame(msgId, msg, flags=canlib.canMSG_EXT)
         #ch0.write(frame)
         print(msgId, msg, dlc, flg, time)
+        print("Coming here")
     except:
 
         try:
