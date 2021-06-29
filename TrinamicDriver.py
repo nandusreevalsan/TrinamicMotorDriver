84668c71c7bfae5922f9f29ada0e6dd220d40854
Nandu Sreevalsan <nandu.sreevalsan@gmail.com>
2021-06-29 11:14:49 +0400

    adding main

--- TrinamicDriver.py
+++ TrinamicDriver.py
@@ -11,50 +11,52 @@ def ChannelParseData(channel):
     print("%d, %s,(%s / %s)" %(channel, ch.channel_name, ch.card_upc_no, ch.card_serial_no))
 
 
-print("Yeah it is comimg here")
+if __name__ == "__main__":
+
+    print("Yeah it is comimg here")
 
 
-channel = 0
-channel1 = 1
+    channel = 0
+    channel1 = 1
 
-#chd = canlib.ChannelData(channel)
-num_channels = canlib.getNumberOfChannels()
+    #chd = canlib.ChannelData(channel)
+    num_channels = canlib.getNumberOfChannels()
 
-print("No of channels: ",num_channels)
+    print("No of channels: ",num_channels)
 
-ChannelParseData(channel)
-ch0 = canlib.openChannel(channel)
-ch0.setBusOutputControl(canlib.canDRIVER_NORMAL)
-ch0.setBusParams(canlib.canBITRATE_1M)
-ch0.busOn()
+    ChannelParseData(channel)
+    ch0 = canlib.openChannel(channel)
+    ch0.setBusOutputControl(canlib.canDRIVER_NORMAL)
+    ch0.setBusParams(canlib.canBITRATE_1M)
+    ch0.busOn()
 
-print("CAN bit rate",canlib.canBITRATE_1M)
-#ch1.busOff()
+    print("CAN bit rate",canlib.canBITRATE_1M)
+    #ch1.busOff()
 
-ChannelParseData(channel1)
-ch1 = canlib.openChannel(channel1)
-ch1.setBusOutputControl(canlib.canDRIVER_NORMAL)
-ch1.setBusParams(canlib.canBITRATE_1M)
-ch1.busOn()
+    ChannelParseData(channel1)
+    ch1 = canlib.openChannel(channel1)
+    ch1.setBusOutputControl(canlib.canDRIVER_NORMAL)
+    ch1.setBusParams(canlib.canBITRATE_1M)   
+    ch1.busOn()
 
-a = True
+    a = True
 
-while a:
-    try:
-        print("before")
-        op.SendCANOpen(ch0)
-        print("after")
-        (msgId, msg, dlc, flg, time) = ch0.read()
-        #frame = Frame(msgId, msg, flags=canlib.canMSG_EXT)
-        #ch0.write(frame)
-        print("Reply:",msgId, msg, dlc, flg, time)
-
-        time.sleep(1)
-    except:
-        print("not able to send the messages")
+    while a:
         try:
-            #frame = Frame(msgId+1, msg, flags=canlib.canMSG_EXT)
-            ch1.write(frame)
+            print("before")
+            op.SendCANOpen(ch0)
+            print("after")
+            (msgId, msg, dlc, flg, time) = ch0.read()
+            #frame = Frame(msgId, msg, flags=canlib.canMSG_EXT)
+            #ch0.write(frame)
+            print("Reply:",msgId, msg, dlc, flg, time)
+
+            time.sleep(1)
         except:
-            a = "Pani paali"
-    a = False
+            print("not able to send the messages")
+            try:
+                #frame = Frame(msgId+1, msg, flags=canlib.canMSG_EXT)
+                ch1.write(frame)
+            except:
+                a = "Pani paali"
+        a = False
